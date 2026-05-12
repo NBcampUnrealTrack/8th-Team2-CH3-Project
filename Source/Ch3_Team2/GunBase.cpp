ceea7add33be6d5183ef028e48b86ce8805af0d1
bowonShin <bowo9777@naver.com>
2026-05-12 17:11:01 +0900

    fix: Weapon
    - 긴급 임시 조치

--- Source/Ch3_Team2/GunBase.cpp
+++ Source/Ch3_Team2/GunBase.cpp
@@ -47,6 +47,47 @@ void AGunBase::Fire_Gun(FVector Location, FVector Direction)
 		UpdateAmmo();
 		return;
 	}
+	// 1. 끝점 계산
+	FVector End = Location + (Direction * EffectiveRange);
+
+	// 2. 파라미터 설정
+	FHitResult HitResult;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(this);           // 자기 자신 제외
+	Params.AddIgnoredActor(GetOwner());    // 소유자(예: 캐릭터) 제외
+
+	// 3. Line Trace 실행
+	bool bHit = GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		Location,
+		End,
+		ECC_Visibility, // 블루프린트의 Visibility 채널
+		Params
+	);
+	FLinearColor LColor =bHit ? FLinearColor::Green : FLinearColor::Red; 
+	DrawDebugLine(
+	GetWorld(),
+	Location,           // 시작 지점
+	End,                // 끝 지점
+	LColor.ToFColor(true),          // 색상
+	false,              // 매 프레임마다 그릴지 여부 (고정된 라인은 false)
+	2.0f,               // 유지 시간 (초 단위, 블루프린트의 For Duration과 대응)
+	0,                  // 우선순위
+	1.0f);            // 두께
+
+	// (옵션) 성공 시 로그 출력 - 테스트용
+	if (bHit)
+	{
+		// 배틀 시스템 호출 샬라샬라
+		AActor* HitActor = HitResult.GetActor();
+		if (HitActor->ActorHasTag("Monster"))
+		{
+			AMonsterBase* Monster = Cast<AMonsterBase>(HitActor);
+		}
+		UE_LOG(LogTemp, Log, TEXT("Hit Actor: %s"), *HitActor->GetName());
+	}
+	
+	
 }
 void AGunBase::Fire(FVector Location, FVector Direction)
 {
