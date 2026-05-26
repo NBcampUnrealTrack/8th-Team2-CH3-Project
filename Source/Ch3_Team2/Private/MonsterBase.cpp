54a002ea808e632e390586fbdf7ae1e3dadccc59
kimdaehyum <129298858+kimdaehyum@users.noreply.github.com>
2026-05-26 10:41:30 +0900

    fix: bug

--- Source/Ch3_Team2/Private/MonsterBase.cpp
+++ Source/Ch3_Team2/Private/MonsterBase.cpp
@@ -83,7 +83,7 @@ void AMonsterBase::HandleDeath(AController* InInstigator,AActor* DeathActor)
 		{
 			
 			STComp->StopLogic(TEXT("Aborted"));
-			STComp->Deactivate();
+			STComp->Deactivate();z
 		}
 	}
 	
